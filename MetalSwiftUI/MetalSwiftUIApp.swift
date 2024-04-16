ab37c551219fea51a2db9c3f6aab31a018959135
Philipp3107 <philippkotte@icloud.com>
2024-04-16 19:11:01 +0200

    Initial Commit

---
+++ MetalSwiftUI/MetalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalSwiftUIApp.swift
+//  MetalSwiftUI
+//
+//  Created by Philipp Kotte on 16.04.24.
+//
+
+import SwiftUI
+
+@main
+struct MetalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
